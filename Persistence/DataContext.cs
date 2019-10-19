1c0987b11b83913e56465edd9e8bc5a6d2723c32
Lucian Zavoianu <lucian.zavoianu@oracle.com>
2019-10-19 12:09:45 +0200

    Added Seed Method and Migration

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,5 +11,14 @@ namespace Persistence
         }
 
         public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>().HasData(
+                new Value { Id = 1, Name = "Value 1" },
+                new Value { Id = 2, Name = "Value 2" },
+                new Value { Id = 3, Name = "Value 3" }
+            );
+        }
     }
 }
