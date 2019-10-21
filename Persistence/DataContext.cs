bc279f9a0dd1ef8509feb7944b7447f5a1d967d2
Lucian Zavoianu <lucian.zavoianu@oracle.com>
2019-10-21 11:58:09 +0200

    Added Events Entity

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,6 +11,7 @@ namespace Persistence
         }
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<Event> Events { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
