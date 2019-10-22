526732f2abe3c79a2c009af31fb0054b9bc84435
Lucian Zavoianu <lucian.zavoianu@oracle.com>
2019-10-22 16:31:23 +0200

    Refactored to Use React Hooks

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,42 +1,33 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Header, Icon, List } from "semantic-ui-react";
-import axios from "axios";
+import axios from 'axios';
 import { IEvent } from "../models/event";
 
-interface IState {
-  events: IEvent[]
-}
+const App = () => {
+  const [events, setEvents] = useState<IEvent[]>([]);
 
-class App extends Component<{}, IState> {
-  readonly state: IState = {
-    events: [],
-  };
-
-  componentDidMount() {
-    axios.get<IEvent[]>('http://localhost:5000/api/events').then((response: any) => {
-      // console.log(response);
-      this.setState({
-        events: response.data
+  useEffect(() => {
+    axios
+      .get<IEvent[]>('http://localhost:5000/api/events')
+      .then((response: any) => {
+        setEvents(response.data);
       });
-    });
-  }
+  }, []); // [] Stops the component from entering a loop, as there is nothing to stop it from running every single time the component will render
 
-  render() {
-    return (
-      <div>
-        <Header as="h2">
-          <Icon name="users" />
-          <Header.Content>Core Events</Header.Content>
-        </Header>
+  return (
+    <div>
+      <Header as="h2">
+        <Icon name="users" />
+        <Header.Content>Core Events</Header.Content>
+      </Header>
 
-        <List>
-          {this.state.events.map((event) => (
-            <List.Item key = { event.id } >{ event.title }</List.Item>
-          ))}
-        </List>
-      </div>
-    );
-  }
-}
+      <List>
+        {events.map(event => (
+          <List.Item key={event.id}>{event.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
+};
 
 export default App;
