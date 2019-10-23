8192a3543c4a40c28467f652a1ae19b5165a8a5b
Lucian Zavoianu <lucian.zavoianu@oracle.com>
2019-10-23 10:15:53 +0200

    Created Events List

--- client/src/features/events/dashboard/EventsDashboard.tsx
+++ client/src/features/events/dashboard/EventsDashboard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Grid, List } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import { IEvent } from "../../../app/models/event";
+import EventsList from "./EventsList";
 
 interface IProps {
     events: IEvent[]
@@ -10,11 +11,12 @@ const EventsDashboard: React.FC<IProps> = ({events}) => {
   return (
     <Grid>
       <Grid.Column width={10}>
-        <List>
+        <EventsList events = { events } />
+        {/* <List>
           {events.map(event => (
             <List.Item key={event.id}>{event.title}</List.Item>
           ))}
-        </List>
+        </List> */}
       </Grid.Column>
     </Grid>
   );
